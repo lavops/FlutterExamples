b79066c3eec0e4888b9b36ba31944c6b2a78ee82
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-27 14:34:52 +0200

    Keep - Notes on Home Page

--- keep/lib/pages/home.dart
+++ keep/lib/pages/home.dart
@@ -31,8 +31,114 @@ class _HomePageState extends State<HomePage> {
         children: [
           // Drawer button, Search, Profile Icon
           Header(size: size, drawerKey: _drawerKey),
+          SizedBox(
+            height: 15,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Text(
+                  "PINNED",
+                  style: TextStyle(
+                    fontSize: 11,
+                    fontWeight: FontWeight.w500,
+                    color: white.withOpacity(0.6),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              buildGrid(),
+            ],
+          ),
         ],
       ),
+    );
+  }
+
+  Widget buildGrid() {
+    var size = MediaQuery.of(context).size;
+
+    return Column(
+      children: List.generate(notes.length, (index) {
+        return Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+          child: Container(
+            width: size.width,
+            decoration: BoxDecoration(
+              color: cardColor,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: white.withOpacity(0.1),
+              ),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 8, right: 8, top: 12, bottom: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Title
+                  Text(
+                    notes[index]['title'],
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: white.withOpacity(0.9),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  // Description
+                  Text(
+                    notes[index]['description'],
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: white.withOpacity(0.7),
+                    ),
+                  ),
+                  // Person Icons
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: List.generate(notes[index]['img'].length,
+                            (indexImg) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 5.0),
+                            child: Container(
+                              width: 25,
+                              height: 25,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: white.withOpacity(0.1),
+                                ),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      notes[index]['img'][indexImg]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
