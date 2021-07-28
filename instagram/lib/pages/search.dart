9617012de21173bc725ffc32feb1fc75466752b5
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-28 09:16:40 +0200

    Instagram - Search Bar & Categories

---
+++ instagram/lib/pages/search.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/json/search_json.dart';
+import 'package:instagram/theme/colors.dart';
+import 'package:instagram/widgets/category.dart';
+
+class SearchPage extends StatefulWidget {
+  SearchPage({Key? key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return ListView(
+      children: [
+        // Search Bar
+        SafeArea(
+          child: Row(
+            children: [
+              SizedBox(
+                width: 15,
+              ),
+              Container(
+                width: size.width - 30,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: textFieldBackground,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefix: Icon(
+                      Icons.search,
+                      color: white.withOpacity(0.5),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: white.withOpacity(0.3),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 15,
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        // Search Categories
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(searchCategories.length, (index) {
+              return Category(name: searchCategories[index]);
+            }),
+          ),
+        ),
+      ],
+    );
+  }
+}
