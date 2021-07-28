9917dd917c6e688bf7d0a01960f683d25036b5a2
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-28 12:10:11 +0200

    Messenger - Chat History

--- messenger/lib/pages/home.dart
+++ messenger/lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:messenger/json/data.dart';
 import 'package:messenger/theme/colors.dart';
+import 'package:messenger/widgets/profile_stack.dart';
 import 'package:messenger/widgets/story.dart';
 
 class HomePage extends StatefulWidget {
@@ -42,6 +43,11 @@ class _HomePageState extends State<HomePage> {
           ),
           // Stories
           buildStories(),
+          SizedBox(
+            height: 30,
+          ),
+          // Chat History
+          buildChatHistory(),
         ],
       ),
     );
@@ -146,6 +152,65 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
+    );
+  }
+
+  Widget buildChatHistory() {
+    var size = MediaQuery.of(context).size;
+
+    return Column(
+      children: List.generate(userMessages.length, (index) {
+        return InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Row(
+              children: [
+                // Image
+                ProfileStack(
+                  isStory: userMessages[index]['story'],
+                  isOnline: userMessages[index]['online'],
+                  profileImg: userMessages[index]['img'],
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                // Name and recent message
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Name
+                    Text(
+                      userMessages[index]['name'],
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    // Recent message
+                    SizedBox(
+                      width: size.width - 135,
+                      child: Text(
+                        userMessages[index]['message'] +
+                            " - " +
+                            userMessages[index]['created_at'],
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: black.withOpacity(0.8),
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
