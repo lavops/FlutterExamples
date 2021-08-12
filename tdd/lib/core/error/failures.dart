9316038d1fed0816bd378d2749f41491b52054db
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-12 09:57:59 +0200

    TDD - (Number Trivia) - Entities, Repository, Error

---
+++ tdd/lib/core/error/failures.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final List<Object> properties;
+
+  Failure({required this.properties});
+
+  @override
+  List<Object> get props => properties;
+}
