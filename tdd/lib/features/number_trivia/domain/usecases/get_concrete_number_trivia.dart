efca47edf7b013a04e8ec5fe6d2479c7f906cc19
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-12 11:07:59 +0200

    TDD - (Number Trivia) - Domain Layer Refactoring

--- tdd/lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ tdd/lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,15 +1,26 @@
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:tdd/core/error/failures.dart';
+import 'package:tdd/core/usecases/usecase.dart';
 import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia extends UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+  Params({required this.number});
+
+  @override
+  List<Object?> get props => [number];
 }
