01a34b39dc2d0368aa4a4b7c761782e373baf666
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-13 08:29:03 +0200

    TDD - (Number Trivia) - Data - Network Info

---
+++ tdd/test/core/network/network_info_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd/core/platform/network_info.dart';
+
+class MockDataConnectionChecker extends Mock
+    implements InternetConnectionChecker {}
+
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to DataConnectionChecker.hasConnection',
+        () async {
+      // arrange
+      final tHasConnectionFailure = Future.value(true);
+      when(() => mockDataConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFailure);
+      // act
+      final result = networkInfo.isConnected;
+      // assert
+      verify(() => mockDataConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFailure);
+    });
+  });
+}
