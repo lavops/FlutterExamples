92f6f036767674031d99f162be0ebfa307199b41
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-13 11:19:28 +0200

    TDD - (Number Trivia) - Presentation - BLoC initial files, Input Converter

---
+++ tdd/test/core/utils/input_converter_test.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd/core/utils/input_converter.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsingedInt', () {
+    test(
+        'should return an integer when the string represents an unsinged integer',
+        () async {
+      // arrange
+      final str = '123';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Right(123));
+    });
+
+    test('should return a failure when the string is not an integer', () async {
+      // arrange
+      final str = 'abc';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+
+    test('should return a failure when the string is negative integer',
+        () async {
+      // arrange
+      final str = '-123';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+  });
+}
