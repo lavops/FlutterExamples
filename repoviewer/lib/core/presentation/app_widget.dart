0ff2fb405fc63c69a064d0b3b2fa4f55b4b2dc5f
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-22 09:57:51 +0200

    Repo Viewer - Navigating Based On the AuthState

--- repoviewer/lib/core/presentation/app_widget.dart
+++ repoviewer/lib/core/presentation/app_widget.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repoviewer/auth/application/auth_notifier.dart';
 import 'package:repoviewer/auth/shared/providers.dart';
 import 'package:repoviewer/core/presentation/routes/app_router.gr.dart';
 
@@ -18,14 +20,34 @@ class AppWidget extends StatelessWidget {
     return ProviderListener(
       provider: initializationProvider,
       onChange: (context, value) {},
-      child: MaterialApp.router(
-        title: 'Repo Viewer',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      child: ProviderListener<AuthSate>(
+        provider: authNotifierProvider,
+        onChange: (context, state) {
+          state.maybeMap(
+            orElse: () {},
+            authenticated: (_) {
+              appRouter.pushAndPopUntil(
+                const StarredReposRoute(),
+                predicate: (route) => false,
+              );
+            },
+            unauthenticated: (_) {
+              appRouter.pushAndPopUntil(
+                const SignInRoute(),
+                predicate: (route) => false,
+              );
+            },
+          );
+        },
+        child: MaterialApp.router(
+          title: 'Repo Viewer',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          routeInformationParser: appRouter.defaultRouteParser(),
+          routerDelegate: appRouter.delegate(),
         ),
-        routeInformationParser: appRouter.defaultRouteParser(),
-        routerDelegate: appRouter.delegate(),
       ),
     );
   }
