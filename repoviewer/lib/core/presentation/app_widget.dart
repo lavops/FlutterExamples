40d52fba68c7ca35a365a03394e4de128e217438
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-22 09:45:26 +0200

    Repo Viewer - Initialization Provider

--- repoviewer/lib/core/presentation/app_widget.dart
+++ repoviewer/lib/core/presentation/app_widget.dart
@@ -1,19 +1,32 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repoviewer/auth/shared/providers.dart';
 import 'package:repoviewer/core/presentation/routes/app_router.gr.dart';
+
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+  return unit;
+});
 
 class AppWidget extends StatelessWidget {
   final appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Repo Viewer',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ProviderListener(
+      provider: initializationProvider,
+      onChange: (context, value) {},
+      child: MaterialApp.router(
+        title: 'Repo Viewer',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routeInformationParser: appRouter.defaultRouteParser(),
+        routerDelegate: appRouter.delegate(),
       ),
-      routeInformationParser: appRouter.defaultRouteParser(),
-      routerDelegate: appRouter.delegate(),
     );
   }
 }
