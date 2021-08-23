cd99a920e3dda32b88839c2d55349e912a572200
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-23 10:07:27 +0200

    Repo Viewer - Changing folder structure

--- repoviewer/lib/core/presentation/routes/app_router.dart
+++ repoviewer/lib/core/presentation/routes/app_router.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:repoviewer/auth/presentation/authorization_page.dart';
 import 'package:repoviewer/auth/presentation/sign_in_page.dart';
 import 'package:repoviewer/splash/presentation/splash_page.dart';
-import 'package:repoviewer/starred_repos/presentation/starred_repos_page.dart';
+import 'package:repoviewer/github/repos/starred_repos/presentation/starred_repos_page.dart';
 
 @MaterialAutoRouter(
   routes: [
