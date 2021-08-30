321665a0dc170784931f27118507b293923dc21b
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-30 12:00:13 +0200

    GSheets - Todos API

--- gsheets/lib/services/gsheets_api.dart
+++ gsheets/lib/services/gsheets_api.dart
@@ -4,23 +4,32 @@ import 'gsheets_credentials.secret.dart';
 
 class GSheetsApi {
   static final _gsheets = GSheets(credentials);
-  static Worksheet? _worksheet;
+  static Worksheet? _worksheetNotes;
+  static Worksheet? _worksheetTodos;
 
+  // NOTES
   static int numberOfNotes = 0;
   static List<String> currentNotes = [];
-  static bool loading = true;
+  static bool loadingNotes = true;
+  // TO-DOs
+  static int numberOfTodos = 0;
+  static List<List<dynamic>> currentTodos = [];
+  static bool loadingTodos = true;
 
   Future init() async {
     final ss = await _gsheets.spreadsheet(spreadsheetID);
-    _worksheet = ss.worksheetByTitle('Proba');
+    _worksheetNotes = ss.worksheetByTitle('NOTES');
+    _worksheetTodos = ss.worksheetByTitle('TODOs');
     // Count number of notes
-    countRows();
+    countRowsNotes();
+    countRowsTodos();
   }
 
-  static Future countRows() async {
-    while (
-        (await _worksheet!.values.value(column: 1, row: numberOfNotes + 1)) !=
-            '') {
+  // NOTES FUNCTIONS
+  static Future countRowsNotes() async {
+    while ((await _worksheetNotes!.values
+            .value(column: 1, row: numberOfNotes + 1)) !=
+        '') {
       numberOfNotes++;
     }
 
@@ -29,25 +38,68 @@ class GSheetsApi {
   }
 
   static Future loadNotes() async {
-    if (_worksheet == null) return;
+    if (_worksheetNotes == null) return;
 
     for (int i = 0; i < numberOfNotes; i++) {
       final String newNote =
-          await _worksheet!.values.value(column: 1, row: i + 1);
+          await _worksheetNotes!.values.value(column: 1, row: i + 1);
 
       if (currentNotes.length < numberOfNotes) {
         currentNotes.add(newNote);
       }
     }
 
-    loading = false;
+    loadingNotes = false;
   }
 
-  static Future insert(String note) async {
-    if (_worksheet == null) return;
+  static Future insertNote(String note) async {
+    if (_worksheetNotes == null) return;
 
     numberOfNotes++;
     currentNotes.add(note);
-    await _worksheet!.values.appendRow([note]);
+    await _worksheetNotes!.values.appendRow([note]);
+  }
+
+  // TO-DO FUNCTIONS
+  static Future countRowsTodos() async {
+    while ((await _worksheetTodos!.values
+            .value(column: 1, row: numberOfTodos + 1)) !=
+        '') {
+      numberOfTodos++;
+    }
+
+    // Load all notes
+    loadTodos();
+  }
+
+  static Future loadTodos() async {
+    if (_worksheetTodos == null) return;
+
+    for (int i = 0; i < numberOfTodos; i++) {
+      final String newTodoString =
+          await _worksheetTodos!.values.value(column: 1, row: i + 1);
+      final int newTodoDone =
+          int.parse(await _worksheetTodos!.values.value(column: 2, row: i + 1));
+      if (currentTodos.length < numberOfTodos) {
+        currentTodos.add([
+          newTodoString,
+          newTodoDone,
+        ]);
+      }
+    }
+
+    loadingTodos = false;
+  }
+
+  static Future insertTodo(String todo) async {
+    if (_worksheetTodos == null) return;
+
+    numberOfTodos++;
+    currentTodos.add([todo, 0]);
+    await _worksheetTodos!.values.appendRow([todo, 0]);
+  }
+
+  static Future updateTodo(int index, int isDone) async {
+    _worksheetTodos!.values.insertValue(isDone, column: 2, row: index + 1);
   }
 }
