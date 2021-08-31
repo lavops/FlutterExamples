08e7feab931d146e87f6c1b9d847ac9e0177c437
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-08-31 21:26:34 +0200

    GSheets - Expenses API

--- gsheets/lib/services/gsheets_api.dart
+++ gsheets/lib/services/gsheets_api.dart
@@ -6,6 +6,7 @@ class GSheetsApi {
   static final _gsheets = GSheets(credentials);
   static Worksheet? _worksheetNotes;
   static Worksheet? _worksheetTodos;
+  static Worksheet? _worksheetExpenses;
 
   // NOTES
   static int numberOfNotes = 0;
@@ -15,11 +16,16 @@ class GSheetsApi {
   static int numberOfTodos = 0;
   static List<List<dynamic>> currentTodos = [];
   static bool loadingTodos = true;
+  // Expenses
+  static int numberOfExpenses = 0;
+  static List<List<dynamic>> currentExpenses = [];
+  static bool loadingExpenses = true;
 
   Future init() async {
     final ss = await _gsheets.spreadsheet(spreadsheetID);
     _worksheetNotes = ss.worksheetByTitle('NOTES');
     _worksheetTodos = ss.worksheetByTitle('TODOs');
+    _worksheetExpenses = ss.worksheetByTitle('EXPENSES');
     // Count number of notes
     countRowsNotes();
     countRowsTodos();
@@ -101,5 +107,77 @@ class GSheetsApi {
 
   static Future updateTodo(int index, int isDone) async {
     _worksheetTodos!.values.insertValue(isDone, column: 2, row: index + 1);
+  }
+
+  // Expenses
+  static Future countRowsExpenses() async {
+    while ((await _worksheetExpenses!.values
+            .value(column: 1, row: numberOfExpenses + 1)) !=
+        '') {
+      numberOfExpenses++;
+    }
+
+    // Load all notes
+    loadExpenses();
+  }
+
+  static Future loadExpenses() async {
+    if (_worksheetExpenses == null) return;
+
+    for (int i = 0; i < numberOfExpenses; i++) {
+      final String transactionName =
+          await _worksheetExpenses!.values.value(column: 1, row: i + 1);
+      final String transactionAmount =
+          await _worksheetExpenses!.values.value(column: 2, row: i + 1);
+      final String transactionType =
+          await _worksheetExpenses!.values.value(column: 3, row: i + 1);
+
+      if (currentExpenses.length < numberOfExpenses) {
+        currentExpenses.add([
+          transactionName,
+          transactionAmount,
+          transactionType,
+        ]);
+      }
+    }
+
+    loadingExpenses = false;
+  }
+
+  static Future insert(String name, String amount, bool _isIncome) async {
+    if (_worksheetExpenses == null) return;
+
+    numberOfExpenses++;
+    currentExpenses.add([
+      name,
+      amount,
+      _isIncome == true ? 'income' : 'expense',
+    ]);
+
+    await _worksheetExpenses!.values.appendRow([
+      name,
+      amount,
+      _isIncome == true ? 'income' : 'expense',
+    ]);
+  }
+
+  static double calculateIncome() {
+    double totalIncome = 0;
+    for (int i = 0; i < currentExpenses.length; i++) {
+      if (currentExpenses[i][2] == 'income') {
+        totalIncome += double.parse(currentExpenses[i][1]);
+      }
+    }
+    return totalIncome;
+  }
+
+  static double calculateExpense() {
+    double totalIncome = 0;
+    for (int i = 0; i < currentExpenses.length; i++) {
+      if (currentExpenses[i][2] == 'expense') {
+        totalIncome += double.parse(currentExpenses[i][1]);
+      }
+    }
+    return totalIncome;
   }
 }
