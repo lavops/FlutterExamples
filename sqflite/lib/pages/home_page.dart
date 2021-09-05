f4776f6f7e701cfde3cefcd87cd45db6e1e329b3
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-09-05 23:55:10 +0200

    Sqflite - Data from database on Home Page

--- sqflite/lib/pages/home_page.dart
+++ sqflite/lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sqflite_demo/database/database.dart';
+import 'package:sqflite_demo/models/note_model.dart';
 import 'package:sqflite_demo/pages/new_note_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<Note>> _noteList;
+  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+  DatabaseHelper _databaseHelper = DatabaseHelper.instance;
+
+  _updateNoteList() {
+    _noteList = DatabaseHelper.instance.getNoteList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _updateNoteList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,44 +49,60 @@ class _HomePageState extends State<HomePage> {
   }
 
   buildBody() {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        if (index == 0) {
-          return Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 40,
-              vertical: 20,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'My Notes',
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  '0 - 10',
-                  style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
+    return FutureBuilder(
+      future: _noteList,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
           );
         }
-        return buildNote(index);
+
+        final int completeNoteCount = snapshot.data!
+            .where((Note note) => note.status == 1)
+            .toList()
+            .length;
+
+        return ListView.builder(
+          itemCount: int.parse(snapshot.data!.length.toString()) + 1,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'My Notes',
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '$completeNoteCount of ${snapshot.data.length}',
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            return buildNote(snapshot.data![index - 1]);
+          },
+        );
       },
     );
   }
 
-  buildNote(int index) {
+  buildNote(Note note) {
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 25,
@@ -77,12 +110,46 @@ class _HomePageState extends State<HomePage> {
       child: Column(
         children: [
           ListTile(
-            title: Text('Note Title $index'),
-            subtitle: Text('Sept 1$index, 2021'),
+            title: Text(
+              note.title!,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+                decoration: note.status == 0
+                    ? TextDecoration.none
+                    : TextDecoration.lineThrough,
+              ),
+            ),
+            subtitle: Text(
+              '${_dateFormatter.format(note.date!)} - ${note.priority}',
+              style: TextStyle(
+                fontSize: 15,
+                decoration: note.status == 0
+                    ? TextDecoration.none
+                    : TextDecoration.lineThrough,
+              ),
+            ),
             trailing: Checkbox(
-              onChanged: (value) {},
+              onChanged: (value) {
+                note.status = value! ? 1 : 0;
+                DatabaseHelper.instance.updateNote(note);
+                _updateNoteList();
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => HomePage(),
+                  ),
+                );
+              },
               activeColor: Theme.of(context).primaryColor,
-              value: true,
+              value: note.status == 1 ? true : false,
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => NewNotePage(),
+              ),
             ),
           ),
           Divider(
